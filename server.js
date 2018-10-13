9e34846d5f34da9767ef7b791f1995e97f16cef7
Henna Kalliokoski <henna.ruohonen@helsinki.fi>
2018-10-13 22:57:59 +0300

    https proxy

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const fs = require('fs')
 const express = require('express')
+const proxy = require('http-proxy-middleware')
+
 
 const index = fs.readFileSync('index.html', 'utf8')
 const style = fs.readFileSync('style.json', 'utf8')
 
-const app = express()
+const app = require('https-localhost')
 
 app.get("/", function(req, res) {
   res.set("Content-Type", "text/html");
@@ -16,6 +18,8 @@ app.get("/style.json", function(req, res) {
   res.send(style);
 });
 
-app.listen(3001, function() {
-  console.log("Listening at localhost:3001");
-});
+app.use("/data", proxy({target: "http://localhost:8080", onProxyReq: (proxyReq, req, res) => {proxyReq.setHeader('x-forwarded-proto', "https")}}))
+
+// app.listen(3001, function() {
+//   console.log("Listening at localhost:3001");
+// });
